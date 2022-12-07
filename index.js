7c0ad8888e28c21ad6ccbf02a2cfcc041e30d283
EllieWalsch <elliewalsch4@gmail.com>
2022-12-07 15:49:52 -0600

    Generate README using data

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown.js');
 
 // TODO: Create an array of questions for user input
 const questions = [{
@@ -48,10 +49,14 @@ const questions = [{
     type:"input",
     name:"email",
     message:"What is your email address?"
-}
-]
+}];
 
-inquirer.prompt(questions);
+inquirer.prompt(questions)
+    .then(data => {
+        fs.writeFileSync("generatedREADME.md", generateMarkdown(data))
+    }
+);
+
 
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}

0e074bcdca1162dd822ac91a41f00ee64847dbde
EllieWalsch <elliewalsch4@gmail.com>
2022-12-07 13:21:08 -0600

    Add section questions

--- index.js
+++ index.js
@@ -3,7 +3,37 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-
+const questions = [{
+    type:"input",
+    name:"title",
+    message:"What is the title of your project?"
+},
+{
+    type:"input",
+    name:"description",
+    message:"Please enter a description of your project."
+},
+{
+    type:"input",
+    name:"installation",
+    message:"What are the steps required to install your project?"
+},
+{
+    type:"input",
+    name:"usage",
+    message:"Please enter description and examples of usage."
+},
+{
+    type:"input",
+    name:"contribution",
+    message:"How can others contribute to this project?"
+},
+{
+    type:"input",
+    name:"tests",
+    message:"What tests can be run on this project? Please provide instructions for use."
+},
+]
 
 // TODO: Create a function to write README file
 // function writeToFile(fileName, data) {}

7ed0aa19d728c3924b7794b57fb49ab08c3624da
EllieWalsch <elliewalsch4@gmail.com>
2022-12-07 22:21:03 -0600

    Render license section
    fix typo, fix license link

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,30 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// function renderLicenseBadge(license) {}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(data) {
-  if (data.license == "No license") {
+function renderLicenseLink(license) {
+  if (license == "No license") {
     return ""
   } else {
-    return `[${data.license}](https://choosealicense.com/licenses/${data.license.toLowerCase().split(' ').join('-')})`
+    return `[${license}](https://choosealicense.com/licenses/${license.toLowerCase().split(' ').join('-')})`
   }
 }
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license == "No License") {
+    return ""
+  } else {
+    return `## License
+
+  ${renderLicenseLink(license)}
+
+    `
+  }
+}
 
 // TODO: Create a function to generate markdown for README
 // badge goes at the top of the page
@@ -43,11 +53,9 @@ function generateMarkdown(data) {
   ## Usage
   
   ${data.usage}
-  
-  ## License
-  
-  ${renderLicenseLink(data)}
 
+  ${renderLicenseSection(data.license)}
+  
   ## Contributing
   
   ${data.contribution}
@@ -59,7 +67,7 @@ function generateMarkdown(data) {
   ## Questions
 
   - Please visit my Github page at [github.com/${data.github}](https://github.com/${data.github})
-  - For additional questions, please [send me and email](mailto:${data.email})
+  - For additional questions, please [send me an email](mailto:${data.email})
 
 `;
 }
